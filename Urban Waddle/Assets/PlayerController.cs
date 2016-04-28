d035698fc5461c2b24d5afd36b7b651e6891056c
harrisna <harrisna@uwec.edu>
2016-04-28 00:00:03 -0500

    WHOOPS FORGOT TO ADD THE TEXTURES AND ALL IN MY PREVIOUS COMMITS I'M BAD AT COMPUTERS BUT WE'RE GOOD NOW
    ALSO: we need to fix some stuff with the "library" folder, we can't ignore it in svn, I'll see what I can do with git.
    I think I may have fixed some stuff with it, but we'll see.  Git isn't good for games.

---
+++ Urban Waddle/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float gravity = 10.0f;
+    public float maxSpeed = 10.0f;
+    public float acceleration = 0.5f;
+
+    CharacterController c;
+
+    // Use this for initialization
+    void Start () {
+        c = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 moveDirection = Vector3.zero;
+
+        if (c.isGrounded)
+        {
+            moveDirection = new Vector3(acceleration * Input.GetAxis("Horizontal"), 0.0f, acceleration * Input.GetAxis("Vertical"));
+
+        }
+        moveDirection.y -= gravity;
+
+        c.Move(moveDirection);
+	}
+}
